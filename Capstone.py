62345743a3f882fe84dc68384d9954a049a3db69
Liam Rowell <liam.rowell.17@cnu.edu>
2021-02-25 13:15:16 -0500

    sample connection working

--- Capstone.py
+++ Capstone.py
@@ -5,13 +5,22 @@ import requests
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly','https://www.googleapis.com/auth/drive.metadata.readonly']
+credentials = service_account.Credentials.from_service_account_file(
+        'Capstone-7383c5975015.json', scopes=SCOPES)
+delegated_credentials = credentials.with_subject('liam.rowell.17@cnu.edu')
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -25,17 +34,32 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = service_account.Credentials.from_service_account_file(
+        'Capstone-7383c5975015.json', scopes=SCOPES)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
+    service = build('sheets', 'v4', credentials=creds)
+
+    #Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId='1zqHDL_biCWzwkFozKYYhoEWc52l1LDD5TpHwG9rlT4g',
+                                range='A1:B4').execute()
+    values = result.get('values', [])
+    
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[1]))
+
     service = build('drive', 'v3', credentials=creds)
 
     # Call the Drive v3 API
-    results = service.files().list(
+    results = service.files().list(q = "'1XlVByPlwLujL36kwCk4JVpaaP4_QqPvk' in parents",
         pageSize=10, fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
 
