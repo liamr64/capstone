c27c21c31414510245848c11091679e8c1eb477c
Liam Rowell <liam.rowell.17@cnu.edu>
2021-03-01 10:05:03 -0500

    began input tool

--- Capstone.py
+++ Capstone.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import pickle
 import os.path
-import requests
+import mysql.connector
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -15,17 +15,41 @@ credentials = service_account.Credentials.from_service_account_file(
 delegated_credentials = credentials.with_subject('liam.rowell.17@cnu.edu')
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+MAIN_DIRECTORY = '1XlVByPlwLujL36kwCk4JVpaaP4_QqPvk'
 SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+LOTTERY_DATA = 'Lottery Data'
+MYSQL_CONFIG = {
+  'user': 'admin',
+  'password': '1387194#',
+  'host': 'database-1.ceb0m91rauea.us-east-1.rds.amazonaws.com',
+  'database': 'Capstone',
+  'raise_on_warnings': True
+}
 
 def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
+    creds = getCreds()
+
+    lotteries = getFiles(MAIN_DIRECTORY, creds)
+
+    if lotteries is None:
+        print('fuckity fuck fuck fuck no lotteries found thats an issue')
+    for lottery in lotteries:
+        print(lottery['name'])
+        lotteryInfo = getFiles(lottery['id'],creds)
+        sendInfo(lotteryInfo, creds)
+
+    
+
+
+def getCreds():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -39,36 +63,48 @@ def main():
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    
+    return creds
 
+def getFiles(parentFile, creds):
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(q ="'"+ parentFile + "'" + " in parents",
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    return items
+
+def getSheets(sheetId,range, creds):
     service = build('sheets', 'v4', credentials=creds)
 
     #Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId='1zqHDL_biCWzwkFozKYYhoEWc52l1LDD5TpHwG9rlT4g',
-                                range='A1:B4').execute()
+    result = sheet.values().get(spreadsheetId=sheetId,
+                                range=range).execute()
     values = result.get('values', [])
-    
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[1]))
+    return values
 
-    service = build('drive', 'v3', credentials=creds)
+def sendQuery(query):
+    conn = mysql.connector.connect(**MYSQL_CONFIG)
+    curA = conn.cursor(buffered=True)
+    curA.execute(query)
+    conn.commit()
+    conn.close()
 
-    # Call the Drive v3 API
-    results = service.files().list(q = "'1XlVByPlwLujL36kwCk4JVpaaP4_QqPvk' in parents",
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
 
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+def sendInfo(lotteryInfo, creds):
+    for doc in lotteryInfo:
+        if doc['name'] == LOTTERY_DATA:
+            data = getSheets(doc['id'], 'A1:B6', creds)
+            tables = 'INSERT INTO Lottery (LotteryName, University,StartTime,timeBetween, numSlots, numTimes) '
+            values = 'VALUES ("%s", "%s","%s",%d, "%s", %d)' % (data[0][1],data[1][1],data[2][1],int(data[3][1]),data[4][1],int(data[5][1]))
+            query = tables + values
+            print(query)
+            sendQuery(query)
+
+
 
 if __name__ == '__main__':
     main()
